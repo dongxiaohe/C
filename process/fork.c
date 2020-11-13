af5f4d519bb2a878068bd91c403e90f3a2b03fa3
Xiaohe Dong <xiaohedong3000@fb.com>
2020-11-13 13:00:55 -0800

    Add fork

--- process/fork.c
+++ process/fork.c
@@ -4,4 +4,7 @@
 int main(int argc, char **argv)
 {
   printf("pid is %d \n", (int)getpid());
+  pid_t newpid = fork();
+  printf("forked pid is %d \n", (int)newpid);
+  printf("new pid is %d \n", (int)getpid());
 }

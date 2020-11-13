7f63a5e8f5adfaed6a8da9eb4810fc656cdee0a0
Xiaohe Dong <xiaohedong3000@fb.com>
2020-11-13 13:09:43 -0800

    Add some logic into fork

--- process/fork.c
+++ process/fork.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char **argv)
 {
   printf("pid is %d \n", (int)getpid());
-  pid_t newpid = fork();
-  printf("forked pid is %d \n", (int)newpid);
-  printf("new pid is %d \n", (int)getpid());
+  pid_t cpid = fork();
+  if (cpid < 0) {
+    perror("something went wrong \n");
+  } else if (cpid == 0) {
+    printf("I am the child process \n");
+    sleep(5);
+    exit(0);
+  }
+  printf("forked pid is %d and waiting it finished \n", (int)cpid);
+  wait(NULL);
+  printf("all done \n");
 }

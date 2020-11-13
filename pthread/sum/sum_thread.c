813fd8a7db4a06b2f8c490857d00a36f55955310
Xiaohe Dong <xiaohedong3000@fb.com>
2020-11-12 17:22:30 -0800

    Create dummy thread to just do print

--- pthread/sum/sum_thread.c
+++ pthread/sum/sum_thread.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <pthread.h>
+
+void *sum(void *arg)
+{
+  char *textArg = (char *)arg;
+  printf("the argument is %s \n", textArg);
+
+  pthread_exit(0);
+}
 
 int main(int argc, char **argv)
 {
-	printf("initial sum");
+  pthread_t tid; 
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+
+  int status = pthread_create(&tid,  &attr, sum, argv[1]);
+
+  printf("pthread_create status is %d \n", status);
+  
+  pthread_join(tid, NULL);
+
+  return 0;
 }

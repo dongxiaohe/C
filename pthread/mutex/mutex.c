ed5c2d3eb3383260a672fa0ef2a579fab8f940b3
Xiaohe Dong <xiaohedong3000@fb.com>
2020-11-13 14:24:50 -0800

    Add two pthread to increment the same global value

--- pthread/mutex/mutex.c
+++ pthread/mutex/mutex.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <pthread.h>
 
+long long sum = 0;
+void* pthread_func(void* args)
+{
+  int increment = *(int *) args;
+  for(int i = 0; i < 5000000; i ++) {
+    sum += increment;
+  }
+  pthread_exit(NULL);
+}
 int main(int argc, char** argv)
 {
-  printf("mutex");
+  pthread_t pid1;
+  int increment1 = 1;
+  pthread_create(&pid1, NULL, pthread_func, &increment1);
+  pthread_join(pid1, NULL); 
+  
+  pthread_t pid2;
+  int increment2 = -1;
+  pthread_create(&pid2, NULL, pthread_func, &increment2);
+  pthread_join(pid1, NULL); 
+  
+  printf("the sum is %lld \n", sum);
 }

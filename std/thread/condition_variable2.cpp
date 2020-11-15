d1bd8380b467f0a74fdd3e0dda70daee74727cbf
Xiaohe Dong <xiaohedong3000@fb.com>
2020-11-15 09:58:46 -0800

    Use mutex to control only one thread is running

--- std/thread/condition_variable2.cpp
+++ std/thread/condition_variable2.cpp
@@ -1,7 +1,31 @@
 #include <thread>
 #include <iostream>
+#include <condition_variable>
+#include <mutex>
 
+struct critical_data {
+  std::mutex m; 
+};
+
+void worker_thread(long long count, critical_data& data)
+{
+  std::unique_lock<std::mutex> ik(data.m);
+  for(;;) {
+    printf("thread starting %lld \n", count);
+    count++;
+  }
+}
 int main()
 {
-  printf("here ");
+  long long count = 0; 
+  critical_data data;
+  std::thread worker1([&]{
+    worker_thread(count, data);
+  });
+  std::thread worker2([&]{
+    worker_thread(count, data);
+  });
+
+  worker1.join();
+  worker2.join();
 }
